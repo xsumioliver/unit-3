fce429ace1e5e19a2efa553a0ad3a9b100ae3028
Xsumi Oliver <32101447+xsumioliver@users.noreply.github.com>
2017-11-13 14:20:26 -0500

    Add files via upload

---
+++ es6 ex1.js
@@ -0,0 +1,10 @@
+
+let makeStory = (name) => {
+    return `Remember, go straight to Grandma's house," 
+    ${name}'s mother cautioned.  "Don't dawdle along the way 
+    and please don't talk to strangers!  
+    The woods are dangerous."
+    
+    "Don't worry, mommy," said ${name}, "I'll be careful.`
+}
+console.log(makeStory("lev"))
